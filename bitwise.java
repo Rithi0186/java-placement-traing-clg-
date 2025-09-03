41a9b443883a7706f2fe814bcd5fcf609e058009
Rithi0186 <rithisubhasasi123@gmail.com>
2025-09-03 15:59:37 +0530

    Create bitwise.java

---
+++ bitwise.java
@@ -0,0 +1,23 @@
+// bitwise operator
+
+class Main {
+    public static void main(String[] args) {
+        int a=4;
+        int b=5;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+         System.out.println(10<<2);
+          System.out.println(10<<3);
+          System.out.println(~-3);
+          System.out.println(~5);
+           System.out.println(10>>2);
+           System.out.println(10>>3);
+           System.out.println(10>>>3);
+
+
+
+
+
+    }
+}
