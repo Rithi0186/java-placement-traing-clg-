c1de39ea1b6da3c37e6ff047767e0b2a849d6d4a
Rithi0186 <rithisubhasasi123@gmail.com>
2025-09-03 11:24:54 +0530

    Create Default.java

---
+++ Default.java
@@ -0,0 +1,13 @@
+class Default {
+    public static void main(String[] args) {
+        long a =2147483648l;//denoting l because default number is int
+        System.out.println(a);
+        float b=65.87f;//default is double so denoting with f
+        System.out.println(b);
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+          
+         
+    }
+}
