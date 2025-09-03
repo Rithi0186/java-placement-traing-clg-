ebad1c76fd83a70b3e22ace7d526f02de9e9a2c6
Rithi0186 <rithisubhasasi123@gmail.com>
2025-09-03 13:12:06 +0530

    Create size.java

---
+++ size.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+          System.out.println(Byte.SIZE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+          System.out.println(Long.SIZE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+         
+    }
+}
