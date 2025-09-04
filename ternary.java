b7d495ecd01c37a7e31b14709e0dbf19a7b2992a
Rithi0186 <rithisubhasasi123@gmail.com>
2025-09-04 15:44:35 +0530

    Create ternary.java

---
+++ ternary.java
@@ -0,0 +1,15 @@
+// ternary operator
+class Main {
+    public static void main(String[] args) {
+        int a=100;
+        int b=45;
+     // if(a>b){
+        //   System.out.println(a);
+           
+       //}
+     //  else{
+     //      System.out.println(b);
+    //   }
+    System.out.println(a>b?a:b);
+    }
+}
