26bd78d1df03d4d20cd965f3a678936b2f6ff3a1
Rithi0186 <rithisubhasasi123@gmail.com>
2025-09-04 15:53:48 +0530

    Create prepos.java

---
+++ prepos.java
@@ -0,0 +1,15 @@
+// operator prefix,postfix
+
+class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a++;
+        int c=20;
+        int d=++c;
+        System.out.println(a);
+     System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
