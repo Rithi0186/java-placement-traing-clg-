73a7a27e479a9cc38975af9718aee02b96edd45b
Rithika <rithisubhasasi123@gmail.com>
2025-09-11 10:21:34 +0530

    Create countin2d.java

---
+++ countin2d.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m =sc.nextInt();
+        int arr[][]=new int[n][m];
+        int count=0;
+        int num=sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+            System.out.println("row" +i);
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(arr[i][j]==num){
+                    count++;
+                    System.out.println("postion ["+i+"] ["+j+"]");
+                }
+            
+            }
+         }
+         System.out.println("count:"+count);
+
+    }
+}
